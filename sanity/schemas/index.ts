d836171254dc1d46f649836789692f960680d1ac
Sergino Martina <serginomartina@icloud.com>
2024-04-12 16:55:26 +0200

    fix

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -8,7 +8,30 @@ import offices from './offices'
 import salesStaff from './sales-staff'
 import pages from './pages'
 import services from './services'
+import {callToActionType} from '../../types/callToActionType'
+import {formType} from '../../types/formType'
+import {heroType} from '../../types/heroType'
+import {imageGalleryType} from '../../types/imageGalleryType'
+import {pageType} from '../../types/pageType'
+import {textWithIllustrationType} from '../../types/textWithIllustrationType'
+import {videoType} from '../../types/videoType'
 
-const schemas = [pages, posts, author, category, projects, vacancies, services, salesStaff, offices, blockContent]
+const schemas = [ pageType,
+    heroType,
+    callToActionType,
+    textWithIllustrationType,
+    imageGalleryType,
+    formType,
+    videoType, 
+    pages, 
+    posts, 
+    author, 
+    category, 
+    projects, 
+    vacancies, 
+    services, 
+    salesStaff, 
+    offices, 
+    blockContent]
 
 export default schemas;
